97cba15b641524e206d87e7bc2b83466423f5df5
dminiac <choudharyashish621@gmail.com>
2022-02-23 12:16:33 +0530

    SampleJava added

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+
+public class Demo1 {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=10; i++){
+            System.out.println("Hello Jenkins");
+        }
+            
+        }
+    }
